cae8ad6b0e465cf3524e452e9ba297774d676451
BucurEva87 <bucur.eva87@gmailcom>
2021-02-04 16:54:35 +0200

    third commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-process.env.CI = false;
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
